4a3c4892d40f1a138c6a9986f6a02c952e030a57
= <=>
2024-07-04 10:34:01 +0400

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.header-container');
 const socialLinks = document.querySelector('.social-links');
 const openBtn = document.querySelector('.hamburger-menu-btn');
 const closeBtn = document.querySelector('.close-btn');
-const overlay = document.querySelector('.overlay');
+const overlay = document.querySelector('header .overlay');
 const menu = document.querySelector('.menu');
 const toggleLanguage = document.querySelector('.language p');
 const languages = document.querySelector('.languages');
@@ -30,66 +30,54 @@ window.addEventListener('scroll', function () {
         header.classList.remove('active-header')
     }
 })
-// openBtn.addEventListener("click", function () {
-//     menu.classList.add('active-menu');
-//     overlay.style.display = "block";
-// })
+// // openBtn.addEventListener("click", function () {
+// //     menu.classList.add('active-menu');
+// //     overlay.style.display = "block";
+// // })
 
-// closeBtn.addEventListener("click", function () {
-//     menu.classList.remove('active-menu')
-//     overlay.style.display = "none";
-// })
+// // closeBtn.addEventListener("click", function () {
+// //     menu.classList.remove('active-menu')
+// //     overlay.style.display = "none";
+// // })
 overlay.addEventListener("click", function () {
     menu.classList.remove('active-menu')
     overlay.style.display = "none";
 })
 
-   
-    
-    function handleNavClick(event) {
-        event.preventDefault();
-        
-        navItems.forEach(nav => nav.parentElement.classList.remove('active-item'));
-        menuLinks.forEach(nav => nav.parentElement.classList.remove('active-item'));
-        
-        this.parentElement.classList.add('active-item');
-        
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-        
-        if (menu.contains(this)) {
-            menu.classList.remove('active-menu'); 
-            overlay.style.display = 'none';
-        }
+
+
+function handleNavClick(event) {
+    event.preventDefault();
+
+    navItems.forEach(nav => nav.parentElement.classList.remove('active-item'));
+    menuLinks.forEach(nav => nav.parentElement.classList.remove('active-item'));
+
+    this.parentElement.classList.add('active-item');
+
+    const targetId = this.getAttribute('href').substring(1);
+    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+
+    if (menu.contains(this)) {
+        menu.classList.remove('active-menu');
+        overlay.style.display = 'none';
     }
-    
-    navItems.forEach(item => item.addEventListener('click', handleNavClick));
-    menuLinks.forEach(item => item.addEventListener('click', handleNavClick));
+}
 
-    openBtn.addEventListener('click', function () {
-        menu.classList.add('active-menu');
-        overlay.style.display = 'block';
-    });
+navItems.forEach(item => item.addEventListener('click', handleNavClick));
+menuLinks.forEach(item => item.addEventListener('click', handleNavClick));
 
-    closeBtn.addEventListener('click', function () {
-        menu.classList.remove('active-menu');
-        overlay.style.display = 'none';
-    });
+openBtn.addEventListener('click', function () {
+    menu.classList.add('active-menu');
+    overlay.style.display = 'block';
+});
 
-    overlay.addEventListener('click', function () {
-        menu.classList.remove('active-menu');
-        overlay.style.display = 'none';
-    });
+closeBtn.addEventListener('click', function () {
+    menu.classList.remove('active-menu');
+    overlay.style.display = 'none';
+});
 
-
-
-// window.addEventListener('scroll', function () {
-//     if (this.window.scrollY >= 500) {
-//         socialLinks.classList.add('fixed-social')
-//     }
-//     else {
-//         socialLinks.classList.remove('fixed-social')
-//     }
-// })
-
+overlay.addEventListener('click', function () {
+    menu.classList.remove('active-menu');
+    overlay.style.display = 'none';
+});
 
